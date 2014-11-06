bdd1e7717d26e831a0e5f01bb2b906976c701c2f
liquidbuffer <liquidbuffer@gmail.com>
2014-11-06 19:05:51 +0100

    Removing python compiled files

--- py/OpenRump/__init__.py
+++ py/OpenRump/__init__.py
@@ -7,7 +7,7 @@ from _open_rump import Game, Ontology
 ###############################################################################
 
 ###############################################################################
-# easier method for getting systems from the world
+# easier method for getting systems from the game object
 ###############################################################################
 
 ontology_system_map = {
@@ -26,3 +26,20 @@ def get_system_helper(self, systemName):
 
 # patch class
 Ontology.World.get_system = get_system_helper
+
+
+class System(object):
+
+    def __init__(self):
+        self.camera_orbit = Ontology.SystemManager.get_camera_orbit
+        self.default_physics_world = Ontology.SystemManager.get_default_physics_world
+        self.input_interface = Ontology.SystemManager.get_input_interface
+        self.loop_timer = Ontology.SystemManager.get_loop_timer
+        self.ogre_dot_scene_loader = Ontology.SystemManager.get_ogre_dot_scene_loader
+        self.ogre_renderer = Ontology.SystemManager.get_ogre_renderer
+        self.third_person_controller = Ontology.SystemManager.get_third_person_controller
+
+###############################################################################
+# pull all systems into namespace of 'Game'
+###############################################################################
+
